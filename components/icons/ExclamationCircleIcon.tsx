f24cf9b54cde2fb9af480621053273f9faa611f6
Kin Dorji <kindorji_tsd@Kins-MacBook-Air.local>
2025-11-24 12:55:36 +0600

    Update icons to accept className prop

--- components/icons/ExclamationCircleIcon.tsx
+++ components/icons/ExclamationCircleIcon.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+
 const ExclamationCircleIcon = ({ className = "w-6 h-6" }: { className?: string }) => (
-<svg className={className} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-</svg>
+    <svg className={className} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+    </svg>
 );
+
 export default ExclamationCircleIcon;

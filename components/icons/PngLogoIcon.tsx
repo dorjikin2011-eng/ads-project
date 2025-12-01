895692522b82cdf5a6e62266a964937e330529c1
Kin Dorji <kindorji_tsd@Kins-MacBook-Air.local>
2025-12-01 15:37:58 +0600

    Update PngLogoIcon to match LoginIcon size and make responsive

--- components/icons/PngLogoIcon.tsx
+++ components/icons/PngLogoIcon.tsx
@@ -1,17 +1,14 @@
-import React from 'react';
+import React from "react";
 
 const PngLogoIcon = () => {
   return (
     <img
       src="/acclogo.png"
-      style={{
-        width: '100%',
-        maxWidth: '150px',
-        height: 'auto',
-        display: 'block',
-      }}
+      alt=""
+      className="w-24 h-24 sm:w-20 sm:h-20 object-contain block"
     />
   );
 };
 
 export default PngLogoIcon;
+
